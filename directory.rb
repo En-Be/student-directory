9402f70035df9593ee6476c4b42f1f2a8b14b922
Nicholas Barnes <n3ttl3t@gmail.com>
2019-04-23 16:49:11 +0000

    make interactive

--- directory.rb
+++ directory.rb
@@ -13,6 +13,19 @@ students = [
   {name: "Norman Bates", cohort: :november}
   ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, enter an empty name"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
   puts  "The students of Villains Academy"
   puts  "-----------------------------"
@@ -29,6 +42,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students."
 end
 
+students = input_students
 print_header
 print_list(students)
 print_footer(students)

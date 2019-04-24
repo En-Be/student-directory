2a6a7a7794439c27ce94af8e70183497d6bbb44f
Nicholas Barnes <n3ttl3t@gmail.com>
2019-04-24 11:54:03 +0000

    add filtered list methods

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# first we print a list of students names
+# first we make a list of students names
 students = [
   {name: "Dr Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -26,23 +26,74 @@ def input_students
   students
 end
 
-def print_header
-  puts  "The students of Villains Academy"
-  puts  "-----------------------------"
+def print_header(selection_message, letter_or_number)
+  puts ""
+  header = {
+    "full" => "(all those enrolled)",
+    "initial" => "(whose name begins with #{letter_or_number})",
+    "length" => "(whose name has less than #{letter_or_number} letters)"
+  }
+  puts "The students of Villains Academy"
+  puts header[selection_message]
+  puts "-----------------------------"
+end
+
+def select_students_with_initial(students, initial)
+  edited_list = []
+  students.each do |student|
+    name_letters = student[:name].split ""
+    if name_letters[0] == initial
+      edited_list << student
+    end
+  end
+  edited_list
+end
+
+def select_students_with_short_name(students, number_of_letters)
+  edited_list = []
+  students.each do |student|
+    if student[:name].length < number_of_letters
+      edited_list << student
+    end
+  end
+  edited_list
 end
 
 def print_list(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1} - #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
+  puts  "-----------------------------"
 # then we print the total number of students
   puts "Overall, we have #{students.count} great students."
+  puts ""
 end
 
-students = input_students
-print_header
-print_list(students)
-print_footer(students)
+#students = input_students
+
+def print_full_list(students)
+  print_header("full", nil)
+  print_list(students)
+  print_footer(students)
+end
+
+def print_filtered_by_initial(students)
+  print_header("initial", "D")
+  filtered_list = select_students_with_initial(students, "D")
+  print_list(filtered_list)
+  print_footer(filtered_list)
+end
+
+def print_filtered_by_name_length(students)
+  print_header("length", 12)
+  filtered_list = select_students_with_short_name(students, 12)
+  print_list(filtered_list)
+  print_footer(filtered_list)
+end
+  
+print_full_list(students)
+print_filtered_by_initial(students)
+print_filtered_by_name_length(students)
